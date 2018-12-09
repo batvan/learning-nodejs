f61f7fd6bb95121fbcb7551343cd3f31cf23ffbd
Van Ly <vangly@gmail.com>
2018-12-08 18:08:49 -0600

    Section 4

---
+++ learning-nodejs-cy_kan/4.1/index.js
@@ -0,0 +1,7 @@
+console.log('The First statement');
+
+setTimeout(function() {
+	console.log('The Second statement');
+}, 2000)
+
+console.log('The Third statement');
---
+++ learning-nodejs-cy_kan/4.2/index.js
@@ -0,0 +1,16 @@
+var fruits = ['apple', 'banana', 'grapes', 'orange'];
+
+for (var i = 0; i < fruits.length; i++) {
+	var d = Math.random(0, 1000) + 1000;
+	setTimeout(function(){
+		console.log(i + '. ' + fruits[i] + ' delayed' + d/1000 + ' seconds');
+	}, d);
+}
+
+
+fruits.forEach(function(fruit, index) {
+	var d = Math.random(0, 1000) + 1000;
+	setTimeout(function() {
+		console.log(index + '. ' + fruit + ' delayed' + d/1000 + ' seconds');
+	}, d);
+});
